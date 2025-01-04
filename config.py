f1627b05ebd9fd346a235ccad69ce81051209fe7
Hilling <1928278661@qq.com>
2025-01-04 10:47:38 +0800

    更新检测

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 # 源 URL 列表
 SOURCE_URLS = [
-    "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt",
+    # "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt",
     "https://anti-ad.net/easylist.txt",
-    "https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/dns.txt",
-    "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/dns.txt",
+    # "https://raw.githubusercontent.com/8680/GOODBYEADS/master/data/rules/dns.txt",
+    # "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/dns.txt",
 
     # 可以在这里添加更多源 URL
 ]
