cf7ec075b564f993648020e89711f41fd4709577
cloudyun233 <1928278661@qq.com>
2025-04-06 09:57:35 +0800

    refactor(config): 更新DNS过滤列表源URL
    将hagezi的DNS过滤列表源URL从pro.txt更改为pro.mini.txt，以减少资源消耗并提高性能

--- config.py
+++ config.py
@@ -5,7 +5,8 @@
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt",
     "https://anti-ad.net/easylist.txt",
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
+    # "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.mini.txt",
 
     # 可以在这里添加更多源 URL
 ]
